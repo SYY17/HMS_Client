8963090bfeb4cc6ba9a206853abfc8dc018dfec3
SYY17 <ysy9808@126.com>
2016-12-04 19:40:04 +0800

    修改UserV(P)O(删除一系列内部类)，抽象出CustomerV(P)O类

---
+++ src/main/java/vo/CustomerVO.java
@@ -0,0 +1,35 @@
+package vo;
+
+public class CustomerVO extends UserVO{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String realName;
+	String phoneNumber;
+	String email;
+	
+	public CustomerVO(int i,String n,String p, String r, String pN, String e) {
+		// TODO Auto-generated constructor stub
+		super(i, n, p);
+		realName = r;
+		phoneNumber = pN;
+		email = e;
+	}
+	
+	public CustomerVO(UserVO uvo, String r, String pN, String e){
+		this(uvo.getID(), uvo.getName(), uvo.getPassword(), r, pN, e);
+	}
+	
+	public String getRealName(){
+		return realName;
+	}
+	
+	public String getPhoneNumber(){
+		return phoneNumber;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+}
--- src/main/java/vo/UserVO.java
+++ src/main/java/vo/UserVO.java
@@ -16,9 +16,9 @@ public class UserVO implements Serializable{//can we add a Userole just like the
 	}
 	
 	public UserVO(int i,String n,String p){
-		id=i;
-		name=n;
-		password=p;
+		id = i;
+		name = n;
+		password = p;
 	}
 	
 	public String getName(){
@@ -32,71 +32,4 @@ public class UserVO implements Serializable{//can we add a Userole just like the
 	public String getPassword(){
 		return password;
 	}
-}
-
-class CustomerPO extends UserVO{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	String address;
-	String phoneNumber;
-	String defaultQuestion;
-	String answerToQues;
-	int credit=0;
-	
-	public CustomerPO(int i,String n,String p,String add,String pn,String d,String ans){
-		super(i,n,p);
-		address=add;
-		phoneNumber=pn;
-		defaultQuestion=d;
-		answerToQues=ans;
-	}
-	
-	public String getAddress(){
-		return address;
-	}
-	
-	public String getPhoneNumber(){
-		return phoneNumber;
-	}
-	
-	public String getDefaultQuestion(){
-		return defaultQuestion;
-	}
-	
-	public String getAnsToAns(){
-		return answerToQues;
-	}
-	
-	public int getCredit(){
-		return credit;
-	}
-}
-
-class HotelWorkerVO extends UserVO{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-}
-
-class WebsitePromoteVO extends UserVO{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-}
-
-class WebsiteManagerVO extends UserVO{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
 }
