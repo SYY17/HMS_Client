057e87ea108113a599f500900d36d1ee2612823b
NathanielLu <854806557@qq.com>
2016-12-08 07:26:10 +0800

    promotionInfo接口的修改

--- src/main/java/businesslogic/orderbl/PromotionInfo.java
+++ src/main/java/businesslogic/orderbl/PromotionInfo.java
@@ -6,13 +6,10 @@ import java.sql.Timestamp;
 public interface PromotionInfo {
 	/**
 	 * 
-	 * @param roomtype,
-	 *            roomNumber, hotelName
+	 * @param hotelName
+	 * @param setTime
+	 * @param initialPrice
 	 * @return 获得最终订单价格
 	 */
-<<<<<<< HEAD
-	public double getFinalPrice(double initialPrice, PromotionVO pvo);
-=======
 	public int getFinalPrice(String hotelName, Timestamp setTime, int initialPrice);
->>>>>>> origin/master
 }
--- src/main/java/businesslogic/promotionbl/PromotionInfoImpl.java
+++ src/main/java/businesslogic/promotionbl/PromotionInfoImpl.java
@@ -1,5 +1,7 @@
 package businesslogic.promotionbl;
 
+import java.sql.Timestamp;
+
 import businesslogic.orderbl.PromotionInfo;
 import vo.PromotionVO;
 
@@ -7,16 +9,24 @@ public class PromotionInfoImpl implements PromotionInfo {
 
 	/**
 	 * 
-	 * @param initialPrice,pvo
+	 * @param initialPrice
+	 * @param hotelName
+	 * @param timeStamp
 	 * @return 获得最终订单价格
 	 */
-	@Override
+	/*@Override
 	public double getFinalPrice(double initialPrice, PromotionVO pvo) {
 		// TODO Auto-generated method stub
 		double finalPrice = -1;
 		finalPrice = pvo.calculatePayment(initialPrice);
 		
 		return finalPrice;
+	}*/
+
+	@Override
+	public int getFinalPrice(String hotelName, Timestamp setTime, int initialPrice) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
