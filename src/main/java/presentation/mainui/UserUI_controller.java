dd8b7483c34e0780ce1be2a7d3ec4ef21d240249
NathanielLu <854806557@qq.com>
2016-12-21 23:59:51 +0800

    可从酒店列表直接下订单

--- src/main/java/presentation/mainui/UserUI_controller.java
+++ src/main/java/presentation/mainui/UserUI_controller.java
@@ -1,6 +1,5 @@
 package presentation.mainui;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import businesslogic.hotelbl.HotelController;
@@ -19,7 +18,9 @@ import presentation.userui.user.AllOrder_start;
 import presentation.userui.user.Credit_start;
 import presentation.userui.user.DetailedInformation_start;
 import presentation.userui.user.HotelInformation_start;
+import presentation.userui.user.MakeOrder_start;
 import vo.HotelVO;
+import vo.RoomVO;
 
 public class UserUI_controller {
 
@@ -69,17 +70,17 @@ public class UserUI_controller {
 	}
 	
 	@FXML
-	private void onHistory(MouseEvent event) throws IOException {
+	private void onHistory(MouseEvent event) throws Exception {
 		Credit_start.getInstance().start(stage);
 	}
 
 	@FXML
-	private void onInfoManage(MouseEvent event) {
+	private void onInfoManage(MouseEvent event) throws Exception  {
 		new DetailedInformation_start().start(stage);
 	}
 	
 	@FXML
-	private void onSearch(MouseEvent event) {
+	private void onSearch(MouseEvent event) throws Exception  {
 		String des = searchTextField.getText();
 		
 		HotelBLService hotelBlService = new HotelController();
@@ -88,7 +89,7 @@ public class UserUI_controller {
 		
 		ArrayList<String> content = new ArrayList<String>();
 		for (int i = 0; i < hvo.size(); i++) {
-			content.add(hvo.get(i).getHotelName());
+			content.add(getExpression(hvo.get(i)));
 		}
 		
 		ObservableList<String> strList = FXCollections.observableArrayList(content);
@@ -96,13 +97,67 @@ public class UserUI_controller {
 	}
 
 	@FXML
-	private void onGo(MouseEvent event) {
+	private void onGo(MouseEvent event)  throws Exception {
 		/*
 		HotelInformation_start a =new HotelInformation_start();
 		a.setName(hotelname);
 		a.start(stage);*/
 		HotelInformation_start.getInstance().setName(hotelname);
 		HotelInformation_start.getInstance().start(stage);
+	}
+	
+	public String getName(String str){
+		String name;
+		int cnt = str.indexOf(" ");
+		name = str.substring(0, cnt);
+		return name;
+	}
+	/**
+	 * 
+	 * @param hvo
+	 * @return listItem
+	 */
+	public String getExpression(HotelVO hvo){
+		String str = hvo.getHotelName();
+		for(int i=0;i<20-hvo.getHotelName().length();i++){
+			str+=" ";
+		}
+		str+=hvo.getBusinessArea();
+		for(int i=0;i<40-str.length();i++){
+			str+=" ";
+		}
+		for(int i=0;i<50-str.length();i++){
+			str+=" ";
+		}
+		str+="星级：";
+		str+=String.valueOf(hvo.getStarLevel());
+		for(int i=0;i<60-str.length();i++){
+			str+=" ";
+		}
+		str+="最低价格：";
+		str+=String.valueOf(getMinPrice(hvo));
+		return str;
+	}
+	
+	
+	public int getMinPrice(HotelVO hvo){
+		HotelBLService hotelBlService = new HotelController();
+		ArrayList<RoomVO> room = hotelBlService.SearchRooms(hvo.getHotelID());
+		int small = 0;
+			for(int j=1;j<room.size();j++){
+				if(room.get(j).getPrice()<room.get(small).getPrice()){
+					small = j;
+				}
+			}
+			int pr = room.get(small).getPrice();
+			return pr;
+	}
+	
+	@FXML
+	private void onOrder(MouseEvent event) throws Exception {
+		MakeOrder_start a = new MakeOrder_start();
+		a.setName(hotelname);
+		a.start(stage);
 	}
 	
 }
